6c5601e81b364af0452aae3fded33ba689440816
Vicente Hernández <vicentehernandezmacia@gmail.com>
2025-09-12 13:56:04 +0200

    docs: added examples and descriptions to category schemas for Swagger

--- app/schemas/category.py
+++ app/schemas/category.py
@@ -1,16 +1,18 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
+
 
 class CategoryBase(BaseModel):
-    name: str
+    name: str = Field(..., example="Food", description="Name of category.")
 
 class CategoryCreate(CategoryBase):
     pass
 
 class CategoryUpdate(CategoryBase):
+    name: str | None = Field(None, example="Kitchen", description="New name of category.")
     pass
 
 class CategoryResponse(CategoryBase):
-    id: int
-    user_id: int
+    id: int = Field(..., example=1,description="ID of category")
+    user_id: int = Field(...,description="User ID")
 
     model_config = {"from_attributes":True}
