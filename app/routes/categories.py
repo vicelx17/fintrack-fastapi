efe0ac60fcb7a7d653b0a2bc81fa446076df3bec
Vicente Hernández <vicentehernandezmacia@gmail.com>
2025-09-12 14:02:09 +0200

    docs: added Swagger documentation for category endpoints

--- app/routes/categories.py
+++ app/routes/categories.py
@@ -17,19 +17,36 @@ router = APIRouter(
 )
 
 
-@router.get("/", response_model=List[CategoryResponse])
-async def list_categories(db:AsyncSession = Depends(get_db), current_user: User = Depends(get_current_user)):
+@router.get("/",
+            response_model=List[CategoryResponse],
+            status_code=status.HTTP_200_OK,
+            summary="List categories",
+            description="Return all authenticated user categories."
+            )
+async def list_categories(db: AsyncSession = Depends(get_db), current_user: User = Depends(get_current_user)):
     return await get_categories(db, current_user.id)
 
-@router.get("/{id}", response_model=CategoryResponse)
+
+@router.get("/{id}",
+            response_model=CategoryResponse,
+            status_code=status.HTTP_200_OK,
+            summary="Get a specific category by ID",
+            description="Return a specific category by ID."
+            )
 async def list_category_by_id(
-        id:int,
-        db:AsyncSession = Depends(get_db),
+        id: int,
+        db: AsyncSession = Depends(get_db),
         current_user: User = Depends(get_current_user)
 ):
     return await get_category_by_id(db, current_user.id, id)
 
-@router.post("/", response_model=CategoryResponse)
+
+@router.post("/",
+             response_model=CategoryResponse,
+             status_code=status.HTTP_201_CREATED,
+             summary="Create new category",
+             description="Allows user to create a category."
+             )
 async def create_new_category(
         category: CategoryCreate,
         db: AsyncSession = Depends(get_db),
@@ -37,7 +54,11 @@ async def create_new_category(
 ):
     return await create_category(db, current_user.id, category)
 
-@router.put("/{id}", response_model=CategoryResponse)
+
+@router.put("/{id}",
+            response_model=CategoryResponse,
+            summary="Update a category",
+            description="Allows user to update a category.")
 async def update_user_category(
         id: int,
         category: CategoryUpdate,
@@ -49,7 +70,10 @@ async def update_user_category(
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Category not found")
     return updated_category
 
-@router.delete("/{id}")
+
+@router.delete("/{id}",
+               summary="Delete a category",
+               description="Allows user to delete a category and all of the associated transactions.")
 async def delete_user_category(
         id: int,
         db: AsyncSession = Depends(get_db),
